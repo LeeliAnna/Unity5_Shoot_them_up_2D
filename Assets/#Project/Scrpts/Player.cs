5f9eb0c8cfe9874947b897d0c6c4ae76b588d47e
Gaelle Timmermans <gaelle.timmermansr@interface3.be>
2025-10-13 16:15:07 +0200

    Modify player bounds

--- Assets/#Project/Scrpts/Player.cs
+++ Assets/#Project/Scrpts/Player.cs
@@ -2,6 +2,7 @@ using Unity.VisualScripting;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
@@ -22,6 +23,11 @@ public class Player : MonoBehaviour
     private float leftLimit;
     private float topLimit;
     private float bottomLimit;
+    private Bounds spriteBounds;
+    private float spriteBoundLeft;
+    private float spriteBoundRight;
+    private float spriteBoundTop;
+    private float spriteBoundBottom;
 
     void Awake()
     {
@@ -30,10 +36,19 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         cam = FindFirstObjectByType<Camera>();
 
+        // récupération de la taille de la caméra
         rightLimit = cam.orthographicSize * cam.aspect;
         leftLimit = -rightLimit;
         topLimit = cam.orthographicSize;
         bottomLimit = -topLimit;
+
+        // Recupération des bords du player
+        spriteBounds = spriteRenderer.bounds;
+        // spriteBoundLeft = spriteBounds.center.x - spriteBounds.extents.x;
+        // spriteBoundRight = spriteBounds.center.x + spriteBounds.extents.x;
+        // spriteBoundTop = spriteBounds.center.y + spriteBounds.extents.y;
+        // spriteBoundBottom = spriteBounds.center.y - spriteBounds.extents.y;
+        
 
         // rightBorder = cameraManager.GetRightBorderPoints(0);
         // leftBorder = cameraManager.GetLeftBorderPoints(0);
@@ -56,11 +71,15 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
+            Debug.Log(spriteBounds.size);
         // Chercher dans bounds du spriteRenderer
         Vector3 position = transform.position;
         if (position.x >= rightLimit)
         {
-            position.x = rightLimit;
+            Debug.Log(spriteBounds.size);
+            Debug.Log(rightLimit - spriteBounds.size.x);
+            //position.x = rightLimit;
+            position.x = rightLimit - spriteBounds.size.x;
         }
         else if (position.y >= topLimit)
         {
